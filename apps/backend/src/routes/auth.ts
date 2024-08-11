f50592087bbc60266bdf6f78a04e6a45432fb17a
Wing Lee <44917547+wingleeio@users.noreply.github.com>
2024-08-11 09:11:25 -0700

    Add register api

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -67,7 +67,7 @@ export const auth = new Hono()
             }
 
             const createSessionQuery = e.insert(e.service.Session, {
-                user: getUserQuery,
+                user: e.select(e.service.User, (u) => ({ filter_single: e.op(u.id, "=", user.id) })),
                 expires_at: e.datetime_current(),
             });
 
@@ -110,5 +110,53 @@ export const auth = new Hono()
             if (!application) {
                 return c.json({ error: "Application not found" }, 404);
             }
+
+            const getUserQuery = e.select(e.service.User, (user) => ({
+                id: true,
+                email: true,
+                filter_single: e.all(
+                    e.set(e.op(user.email, "=", body.email), e.op(user.application.id, "=", e.uuid(app))),
+                ),
+            }));
+
+            const user = await getUserQuery.run(client);
+
+            if (user) {
+                return c.json({ error: "User with this email already exists" }, 400);
+            }
+
+            const hashedPassword = await hash(body.password, {
+                memoryCost: 19456,
+                timeCost: 2,
+                outputLen: 32,
+                parallelism: 1,
+            });
+
+            const createUserQuery = e.insert(e.service.User, {
+                email: body.email,
+                hashed_password: hashedPassword,
+                application: getApplicationQuery,
+            });
+
+            const getCreatedUserQuery = e.select(createUserQuery, () => ({
+                id: true,
+                email: true,
+            }));
+
+            const createdUser = await getCreatedUserQuery.run(client);
+
+            const createSessionQuery = e.insert(e.service.Session, {
+                user: e.select(e.service.User, (u) => ({ filter_single: e.op(u.id, "=", createdUser.id) })),
+                expires_at: e.datetime_current(),
+            });
+
+            const getSessionQuery = e.select(createSessionQuery, () => ({
+                id: true,
+                expires_at: true,
+            }));
+
+            const session = await getSessionQuery.run(client);
+
+            return c.json({ session });
         },
     );
