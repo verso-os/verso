2eae678337f1f047c5871897c64b4efc00a15934
Dylan Reed <dylan.reeder5@gmail.com>
2024-08-23 17:13:44 -0400

    add links to navbar

--- apps/web/src/app/applications/[app]/layout.tsx
+++ apps/web/src/app/applications/[app]/layout.tsx
@@ -5,25 +5,50 @@ import { ModeToggle } from "$web/components/mode-toggle";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { useState } from "react";
 
 export default function ApplicationLayout({ children }: { children: React.ReactNode }) {
     const params = useParams<{ app: string }>();
+
+    const [links] = useState([
+        {
+            title: "Management",
+            links: [
+                {
+                    title: "Users",
+                    href: `/applications/${params.app}/users`,
+                },
+                {
+                    title: "Organizations",
+                    href: `/applications/${params.app}/organizations`,
+                },
+            ],
+        },
+        {
+            title: "Configure",
+            links: [
+                {
+                    title: "Webhooks",
+                    href: `/applications/${params.app}/webhook`,
+                },
+                {
+                    title: "OAuth",
+                    href: `/applications/${params.app}/oauth`,
+                },
+                {
+                    title: "Settings",
+                    href: `/applications/${params.app}/settings`,
+                },
+            ],
+        },
+    ]);
+
     return (
         <div className="flex flex-col h-screen">
             <ApplicationBreadcrumbs />
             <div className="grid grid-cols-12 flex-grow">
                 <nav className="col-span-3 bg-primary-foreground p-4 border-r-[0.5px] flex flex-col justify-between">
-                    <ul className="flex flex-col gap-4 text-primary">
-                        {/* <li>
-                            <Link href="/applications" className="flex items-center gap-2">
-                                <ChevronLeft className="h-4 w-4" />
-                                <span>Back</span>
-                            </Link>
-                        </li> */}
-                        <li>
-                            <Link href={`/applications/${params.app}/users`}>Users</Link>
-                        </li>
-                    </ul>
+                    <Tree links={links} />
                     <div>
                         <ModeToggle />
                     </div>
@@ -33,3 +58,30 @@ export default function ApplicationLayout({ children }: { children: React.ReactN
         </div>
     );
 }
+
+const Tree = ({ links }: { links: any[] }) => {
+    return (
+        <ul className="flex flex-col gap-4 text-primary">
+            {links.map((link) => (
+                <TreeItem key={link.title} link={link} />
+            ))}
+        </ul>
+    );
+};
+
+const TreeItem = ({ link }: { link: any }) => {
+    return (
+        <li>
+            <span className="font-medium">{link.title}</span>
+            {link.links && (
+                <ul className="ml-2">
+                    {link.links.map((subLink: any) => (
+                        <li key={subLink.title}>
+                            <Link href={subLink.href}>{subLink.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </li>
+    );
+};
